1aba823ca1a3b87eb371268ab7dcb4030e1021f1
Abhishek Mishra <abhi.mishra509@gmail.com>
2022-08-01 15:18:25 +0530

    initial commit

---
+++ if_elif_selection/Q1.sh
@@ -0,0 +1,36 @@
+read -p "enter the single digit number : " number
+case $number in 
+1)
+    echo "sandeep"
+;;
+2)
+    echo "sanjay"
+;;
+3)
+    echo "rajeev"
+;;
+4)
+    echo "rochak"
+;;
+5)
+    echo "shailendra"
+;;
+6)
+    echo "ashish"
+;;
+7)
+    echo "Seven"
+;;
+8)
+    echo "Eight"
+;;
+9)
+    echo "Nine"
+;;
+0)
+    echo "Zero"
+;;
+*)
+    echo "Your input in invalid, try again"
+;;
+esac
